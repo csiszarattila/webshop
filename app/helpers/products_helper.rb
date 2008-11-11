5b73c1848a75bb55716f24077f744be6235f27f6
Attila Csiszár <csiszar.ati@gmail.com>
2008-11-11 10:20:37 +0100

    Added image_for_product and price_for_product to ProductsHelper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,14 @@
 module ProductsHelper
+	def image_for_product(product)
+		image_tag( product.images.first.image_url, 
+			:size => "150x150", 
+			:alt => product.images.first.description )
+	end
+	
+	def price_for_product(product)
+		number_to_currency( 1245, 
+			:unit => "Ft", 
+			:delimiter => ",", 
+			:format => "%n <span class='currency'>%u</span>" )
+	end
 end
