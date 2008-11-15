6bc170631c90fdbdd7258ab2819c6a7c4a659d4b
Attila Csiszár <csiszar.ati@gmail.com>
2008-11-15 14:45:04 +0100

    Added MyFormBuilder#radio_group

--- app/helpers/my_form_builder.rb
+++ app/helpers/my_form_builder.rb
@@ -38,7 +38,7 @@ class MyFormBuilder < ActionView::Helpers::FormBuilder
 			args.merge!( { :size => "20" }) unless args[:size]
 			wrapper_div_classes = ""
 			
-			label = label(field,args[:label])
+			label = label(field,args.delete(:label))
 
 			wrapper_div_classes << INPUT_CLASS_NAME
 			wrapper_div_classes << " #{ERROR_CLASS_NAME}" unless @object.errors.on(field).nil?
@@ -57,5 +57,57 @@ class MyFormBuilder < ActionView::Helpers::FormBuilder
     contents << "<p>#{options[:message]}</p>" unless options[:message].blank?
     contents << @object.errors.full_messages.map {|msg| "<p>#{msg}</p>" }.join
 		contents << "</div>"
+	end
+	
+	
+	# Wrap around a radio button group in such a way:
+	# 
+	# <legend class="as-label">options[:legend]</legend>
+	# <div class="radio-group">
+	# 	...&block return value...
+	# </div>
+	# 
+	# The wrapper div's content is the return value of the block
+	# 
+	# The block got the radio_group_options one by one, so you can
+	# fetch elements and decide which attribute will be for example
+	# the label tag or anyhing.
+	#
+	# And of course it checks the error messages and adds the appropriate css classes 
+	# Use it at forms as:
+	# = f.radio_group :order_type, @order_types, :legend => "Fizetési és szállítási mód" do |ot|
+	# 	- radio_button("order", "order_type_id", ot.id) + f.label(:order_type, ot.name)
+	# 
+	# 
+	# A blokként átadott radio gombokat körülveszi a megadott módon. 
+	# Minden +radio_group_options+ -ra meghivja a kapott blokkot egyesével, 
+	# a blokk paraméterként megkapja a radio_group_options elemeit,
+	# a blokk visszatérési értékét pedig összeilleszti, és becsomagolja a megfelelő formátumba:
+	# 
+	# <legend class="as-label">options[:legend]</legend>
+	# <div class="radio-group">
+	# 	...&block visszatérési értéke...
+	# </div>
+	# 
+	# Így a formban csak ennyit kell tenni:
+	# 
+	# = f.radio_group :order_type, @order_types, :legend => "Fizetési és szállítási mód" do |ot|
+	# 	- radio_button("order", "order_type_id", ot.id) + f.label(:order_type, ot.name)
+	# 
+	def radio_group(method,radio_group_options,options = {}, &blk)
+		legend_class = "" << "as-label"
+		legend_class << "as-label-with-error" if @object.errors.invalid?(method)
+		
+    wrapper_div_classes = "" << "radio-group"
+		wrapper_div_classes << " radio-group-with-error" if @object.errors.invalid?(method)
+		
+		radio_group_contents = ""
+		for option in radio_group_options
+			radio_group_contents << yield(option)
+			radio_group_contents << "<br/>"
+		end
+		
+		output = @template.content_tag(:legend, options[:legend], :class => legend_class)
+		output << @template.content_tag(:div,radio_group_contents, :class => wrapper_div_classes)
 	end
 end
