23fd2ebc5031b3b6543bc75b646ea072e5aef8f4
Attila Csiszár <csiszar.ati@gmail.com>
2008-11-16 15:34:28 +0100

    CartController test added

--- test/functional/cart_controller_test.rb
+++ test/functional/cart_controller_test.rb
@@ -1,8 +1,53 @@
 require 'test_helper'
 
 class CartControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+	
+	def setup
+		@sony_tv = products(:sony_tv)
+		@keane_cd = products(:keane_cd)
+		
+		@cart = Cart.new()
+		@cart.add_product @sony_tv
+		@cart.add_product @sony_tv
+		@cart.add_product @keane_cd
+	end
+	
+	test "cart created when first product added" do
+		get :add, {:id => Product.first}
+
+		assert_not_nil assigns(:cart)
+		assert_not_nil session[:cart]
+		assert_redirected_to :show_cart
+	end
+	
+	test "empty cart" do
+		get :empty, nil, { :cart => @cart }
+		assert_nil session[:cart]
+		assert_redirected_to :root
+		assert_equal I18n.t('cart.emptied'), flash[:notice]
+	end
+	
+	test "remove product from cart" do
+		get :remove, {:id => @sony_tv.id}, {:cart => @cart }
+		
+		assert_not_nil session[:cart]
+		assert_equal 1, session[:cart].items[0].quantity, session[:cart]
+		assert_redirected_to :show_cart
+	end
+	
+	test "destroy a cart item" do
+		get :destroy, {:id => @sony_tv.id}, {:cart => @cart}
+		
+		assert_not_nil session[:cart]
+		assert_equal 1, session[:cart].items.size, "Cart: #{session[:cart]}"
+		assert_redirected_to :show_cart
+		assert_equal I18n.t('cart.item.destroyed'), flash[:notice]
+	end
+	
+	test "empty cart if destroyed or removed when one item was in it" do
+		get :destroy, {:id => @sony_tv.id}, {:cart => @cart} # remove all items[0]
+		get :remove, {:id => @keane_cd.id}, {:cart => @cart} # remove the one items[1]
+		
+		assert_redirected_to :empty_cart
+	end
 end
--- test/unit/order_test.rb
+++ test/unit/order_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+	
+  test "add items from cart" do
+		cart = Cart.new()
+		[:marple_book, :bob_dvd, :keane_cd].each { |name| cart.add_product(products(name)) }
+		order = Order.new()
+		order.add_items_from_cart(cart)
+		
+		assert_not_nil order.items
+		assert_equal cart.items.size, order.items.size
+		assert_equal cart.total_price, order.total_price
   end
 end
