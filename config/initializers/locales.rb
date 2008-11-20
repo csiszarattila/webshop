c9cc9b58370a5b21aea66e2fcae2fc3cbd4ae35d
Attila Csiszár <csiszar.ati@gmail.com>
2008-11-20 16:27:43 +0100

    Show loaded locale files in log

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,3 +1,8 @@
-I18n.load_path += Dir[ File.join(RAILS_ROOT, 'app', 'locales', '*.{rb,yml}') ]
+load_path = Dir[ File.join(RAILS_ROOT, 'app', 'locales', '*.{rb,yml}') ]
+
+load_path.each do |file|
+	Rails.logger.info("** [locales] Loaded locales file: #{file}")
+end
+I18n.load_path += load_path
 
 I18n.default_locale = "hu-HU"
