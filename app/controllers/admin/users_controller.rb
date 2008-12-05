2bb2f86e5142702c65abaaa009941ebad3d8343e
Attila Csiszár <csiszar.ati@gmail.com>
2008-12-05 22:15:08 +0100

    user handling in admin interface

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,41 @@
-class Admin::UserController < AdminController
+class Admin::UsersController < AdminController
   def index
-		@users = User.admins
+		@users = UserGroup.find_by_name("admin").users
   end
 
   def edit
+		@user = User.find(params[:id])
   end
 
   def new
-  end
-
-  def update
-  end
-
-  def destroy
+		@user = User.new()
   end
 
   def create
+		@user = User.create_an_admin(params[:user])
+		if @user.valid?
+			flash[:notice] = I18n.t 'user.created'
+			redirect_to admin_users_path
+		else
+			render :action => "new"
+		end
+  end
+
+  def update
+		@user = User.find(params[:id])
+		if @user.update_attributes params[:user]
+			flash[:notice] = I18n.t 'user.updated'
+			redirect_to admin_users_path
+		else
+			render :action => "edit"
+		end
+  end
+
+  def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:notice] = I18n.t 'user.destroyed'
+		redirect_to :action => "index"
   end
 
 end
