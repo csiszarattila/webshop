ff52780061c8e07b8be8ac48dcc8ba1ae31e444d
Attila Csiszár <csiszar.ati@gmail.com>
2008-12-06 17:31:48 +0100

    category must be exists if product is created

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,10 +2,11 @@ class Product < ActiveRecord::Base
 	belongs_to :category
 	has_many :attrs, 	:class_name => "ProductAttribute"
 	has_many :category_attributes, :through => :attrs
-	has_and_belongs_to_many :tags
 	has_many :images, :class_name => "ProductImage"
+	has_and_belongs_to_many :tags
 
-	validates_presence_of 		:name, :price, :category_id
+	validates_presence_of :name, :price, :category_id
+	validates_existence_of :category
 	# Minimun ar >= 1 Ft es egesz szam
 	# Minumun price must be >= 1 Ft and must be integer
 	validates_numericality_of :price, :only_integer => true, :greater_than_or_equal_to => 1
