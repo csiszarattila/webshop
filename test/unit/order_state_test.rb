23b6c4de3bed48afcab31c511f45b19ea8c5fd31
Attila Csiszár <csiszar.ati@gmail.com>
2008-12-06 13:08:52 +0100

    order_state's fixture :new renamed to :new_order, use this in tests

--- test/unit/order_state_test.rb
+++ test/unit/order_state_test.rb
@@ -9,7 +9,7 @@ class OrderStateTest < ActiveSupport::TestCase
 	
   test "order state must be unique" do
 		order_state = OrderState.new()
-		order_state.name = order_states(:new).name
+		order_state.name = order_states(:new_order).name
 		
 		assert !order_state.valid?
 		assert_equal I18n.translate('activerecord.errors.messages.taken'), order_state.errors.on(:name)
