8b16c9d4bc48059f830866b8155cfc017376ce3c
Attila Csiszár <csiszar.ati@gmail.com>
2008-12-08 21:30:59 +0100

    Restore deleted image after test

--- test/unit/product_image_test.rb
+++ test/unit/product_image_test.rb
@@ -2,10 +2,20 @@ require 'test_helper'
 
 class ProductImageTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "destroy delete image from disk" do
+  test "destroy is delete image from disk" do
 		image = product_images(:rails_book_picture)
 		file_path = File.join(RAILS_ROOT,'public',image.image_url)
 		image.destroy
 		assert !File.exist?(file_path), file_path
   end
+
+	def teardown
+		# Restore deleted image
+		# Törölt kép visszaállítása
+		image_url = product_images(:rails_book_picture).image_url_before_type_cast
+		FileUtils.cp (
+			File.join(RAILS_ROOT, 'test', 'fixtures', 'images', image_url), 
+			File.join(RAILS_ROOT, 'public', product_images(:rails_book_picture).image_url)
+		)
+	end
 end
