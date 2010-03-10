060115072384a44a28e69efcacea283a2456fa32
Attila Csiszár <csiszar.ati@gmail.com>
2010-03-10 21:39:28 +0100

    Cleneaning up category listing in templates

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,11 +1,2 @@
 module CategoriesHelper
-	def nested_list_for_categories(categories)
-		return "" if categories.empty?
-		out = ""
-		out += "<li class='current'>"
-		category = categories.pop
-		out += link_to(category.name, category_path(category))
-		out += nested_list_for_categories(categories)
-		out += "</li>"
-	end
 end
