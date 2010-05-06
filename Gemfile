7ff9d827f389e07bd6c510462e97fd45548f19cd
Attila Csiszár <csiszar.ati@gmail.com>
2010-05-06 22:46:20 +0200

    Using bundler...yeah

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org/'
+gem "rails", "~> 2.3.5"
+gem "mysql" 
+gem "zipcodematch"
+#gem "sqlite3-ruby", :require => "sqlite3"
+gem "haml"
+gem "will_paginate", '~> 2.3.6'
+gem "mini_magick"
--- config/environment.rb
+++ config/environment.rb
@@ -21,16 +21,6 @@ Rails::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Specify gems that this application depends on. 
-  # They can then be installed with "rake gems:install" on new installations.
-  # You have to specify the <tt>:lib</tt> option for libraries, where the Gem name (<em>sqlite3-ruby</em>) differs from the file itself (_sqlite3_)
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-	config.gem "haml"
-	config.gem "mislav-will_paginate", :version => '~> 2.3.6', :lib => 'will_paginate', :source => 'http://gems.github.com'
-  config.gem "mini_magick"
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
